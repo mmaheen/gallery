7b99187da643d2d1a4062785604df514732595ae
mmaheen <mushrifmaheen@gmail.com>
2025-04-24 11:26:48 +0600

    Photo Model & Migration creation

---
+++ database/migrations/2025_04_24_051149_create_photos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->id();
+            $table->unSignedBigInteger('user_id'); //For foreign key unsiged ig integer
+            $table->string('photo');
+            $table->text('description');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('photos');
+    }
+};
