e0102bbe94cd16e7423a10db70de591870276fee
mmaheen <mushrifmaheen@gmail.com>
2025-04-26 03:01:11 +0600

    Minor bug fix

--- resources/views/backend/photo/create.blade.php
+++ resources/views/backend/photo/create.blade.php
@@ -6,7 +6,6 @@
 
 @section('content')
     <div class="col-12 mb-5">
-        <h2 class="tm-text-primary mb-5">Contact Page</h2>
         <form id="contact-form" action="{{route('photo.store')}}" method="POST" class="tm-contact-form mx-auto" enctype="multipart/form-data">
             @csrf
 
