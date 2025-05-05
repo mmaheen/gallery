a3cd800925695c35f1b65dc027268fad9197a65a
mmaheen <mushrifmaheen@gmail.com>
2025-05-05 16:15:29 +0600

    Views

--- resources/views/frontend/show/photo.blade.php
+++ resources/views/frontend/show/photo.blade.php
@@ -37,6 +37,9 @@
                         <div class="mr-4 mb-2">
                             <span class="tm-text-gray-dark">Format: </span><span class="tm-text-primary">{{$photo_format}}</span>
                         </div>
+                        <div class="mr-4 mb-2">
+                            <span class="tm-text-gray-dark">Views: </span><span class="tm-text-primary">{{$photo->views}}</span>
+                        </div>
                     </div>
                     <div class="mb-4">
                         <h3 class="tm-text-gray-dark mb-3">{{$photo->category->name}}</h3>
