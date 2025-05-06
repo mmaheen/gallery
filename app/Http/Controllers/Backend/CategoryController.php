b3cb29e4c149bb64ca5fd2b92dbb3bac20a07e1d
mmaheen <mushrifmaheen@gmail.com>
2025-05-06 09:30:46 +0600

    Category table Eager Loading

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -14,7 +14,7 @@ class CategoryController extends Controller
     public function index()
     {
         //
-        $categories = Category::all();
+        $categories = Category::with('user')->get();
         return view('backend.category.table',compact('categories'));
     }
 
